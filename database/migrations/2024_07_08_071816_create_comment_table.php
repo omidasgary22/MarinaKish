5797274145e387f887539b96257ede272114e830
unknown <omidasgary966@gmail.com>
2024-07-08 11:03:46 +0330

    create comment migration

---
+++ database/migrations/2024_07_08_071816_create_comment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comment', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unique(['user_id', 'product_id']);
+            $table->string('titel');
+            $table->text('body');
+            $table->enum('star', ['1', '2', '3', '4', '5']);
+            $table->enum('status', ['Accepted', 'failed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment');
+    }
+};
