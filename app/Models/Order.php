c1a7e0ea9e2134c8c3b98caa42205ca2df9b1c04
mohammad <mohammadelia30@gmail.com>
2024-07-08 11:00:07 +0330

    make order model

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable =[
+        'user_id',
+        'status',
+        'beath',
+        'factor_id'
+    ];
+    protected $cast =[
+        'user_id'=> 'integer',
+        'status'=>'enum',
+        'beath'=>'datetime',
+        'factor_id'=>'integer'
+    ];
+}
