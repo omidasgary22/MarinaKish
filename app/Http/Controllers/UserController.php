a695f08367368b24f232212712dc8a4e9f0b84ee
unknown <omidasgary966@gmail.com>
2024-07-08 09:34:00 +0330

    edit migration user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $User = User::create($request->toArray());
+        return response()->json($User);
+
+    }
+
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username');
+            $table->string('national_code');
             $table->string('email')->nullable();
             $table->string('phone')->unique();
             $table->string('password');
