83a60b05c123a48b39702ce387ecc6e2618efa62
unknown <mohammadelia30@gmail.com>
2024-07-09 19:29:15 +0330

    make migration for off code

---
+++ database/migrations/2024_07_09_154758_create_offcodes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offcodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('code');
+            $table->integer('percent');
+            $table->integer('number');
+            $table->datetimes('expire_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offcodes');
+    }
+};
