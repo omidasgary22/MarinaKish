5e1b4f2a91e8ad1727cf8fb15b26644796574b4c
unknown <mohammadelia30@gmail.com>
2024-07-09 18:52:06 +0330

    edit setting table

--- database/migrations/2024_07_08_082905_create_settings_table.php
+++ database/migrations/2024_07_08_082905_create_settings_table.php
@@ -12,11 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('settings', function (Blueprint $table) {
-            $table->id();
             $table->string('key');
             $table->text('value');
             $table->enum('type', ['json', 'string']);
-            $table->timestamps();
         });
     }
 
