8022f11aabb7c51c82da98ad9727c625bd5cafcc
mohammad <mohammadelia30@gmail.com>
2024-07-11 15:11:45 +0330

    make register and login route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register',[UserController::class,'create'])->name('register');
+Route::post('login',[UserController::class,'login'])->name('login');
 
