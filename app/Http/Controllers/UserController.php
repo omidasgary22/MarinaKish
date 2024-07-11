a92313af4415f951312083d8fb4a5d0346f540eb
mohammad <mohammadelia30@gmail.com>
2024-07-11 11:06:41 +0330

    make login and rejister method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,14 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function login(Request $request){
+        $user = new User();
+        $national_code = $request->national_code;
+        $password = $request->password;
+        $code = $user->select('id','national_code','password')->where('national_code',$national_code)->first();
+        $password = Hash::check($password,$user->password);
+        if(!$code|!$password){
+            return response()->json('password or national code wrong');
+        }
+    }
     public function create(Request $request)
     {
-        $User = User::create($request->toArray());
-        return response()->json($User);
-
+        $user = new User();
+        $user->create($request->toArray());
+        return response()->json();
     }
 
 
