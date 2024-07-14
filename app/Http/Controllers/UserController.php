37dd62fe04d5e4b2042bf9070259926ce14275d5
mohammad <mohammadelia30@gmail.com>
2024-07-14 16:30:29 +0330

    make login function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,15 +5,26 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function login(Request $request)
+    {
+        $code = $request->national_code;
+        $password = $request->password;
+        $user = User::select('id', 'national_code', 'password')->where('national_code', $code)->first();
+        if (!$user) {
+            return response()->json('national_code wrong');
+        }if(!Hash::check($password ,$user->password)){
+            return response()->json('password wrong');
+        }
+        $token = $user->createToken($code)->plainTextToken;
+        return response()->json(['token'=>$token]);
+    }
     public function create(Request $request)
     {
-        $User = User::create($request->toArray());
+        $User = User::create($request->merge(["password" => Hash::make($request->password)])->toArray());
         return response()->json($User);
-
     }
-
-
 }
