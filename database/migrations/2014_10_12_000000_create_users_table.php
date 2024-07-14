c5c13d4bfe1b0b435cc320b5e7119b9ae83fb06b
mohammad <mohammadelia30@gmail.com>
2024-07-14 16:31:39 +0330

    edit column for table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->nullable();
             $table->string('national_code');
             $table->string('email')->nullable();
             $table->string('phone')->unique();
