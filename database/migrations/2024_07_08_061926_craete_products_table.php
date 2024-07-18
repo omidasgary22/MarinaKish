d335bc8707400592d1d170f5e2f3917bd3f211f9
mohammad <mohammadelia30@gmail.com>
2024-07-18 09:08:11 +0330

    add off suggestion column

--- database/migrations/2024_07_08_061926_craete_products_table.php
+++ database/migrations/2024_07_08_061926_craete_products_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->integer('total');
             $table->integer('remaning');
             $table->text('description');
+            $table->text('tip');
+            $table->enum('off_suggestion',['yes','no']);
             $table->time('start_time');
             $table->time('end_time');
             $table->timestamps();
