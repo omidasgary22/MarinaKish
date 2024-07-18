aec41cf27158202b129c9c8cc4cc4b28f4519d6a
mohammad <mohammadelia30@gmail.com>
2024-07-18 09:48:40 +0330

    add name column for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('national_code');
             $table->string('email')->nullable();
             $table->string('phone')->unique();
