04aaa2f48a1c2a36a7df7083a0663e0c73782735
mohammad <mohammadelia30@gmail.com>
2024-07-18 12:30:31 +0330

    fix merge error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,23 +10,6 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-<<<<<<< HEAD
-    public function login(Request $request){
-        $user = new User();
-        $national_code = $request->national_code;
-        $password = $request->password;
-        $code = $user->select('id','national_code','password')->where('national_code',$national_code)->first();
-        $password = Hash::check($password,$user->password);
-        if(!$code|!$password){
-            return response()->json('password or national code wrong');
-        }
-    }
-    public function create(Request $request)
-    {
-        $user = new User();
-        $user->create($request->toArray());
-        return response()->json($request);
-=======
     public function login(Request $request)
     {
         $code = $request->national_code;
@@ -44,6 +27,5 @@ class UserController extends Controller
     {
         $User = User::create($request->merge(["password" => Hash::make($request->password)])->toArray());
         return response()->json($User);
->>>>>>> mohammad
     }
 }
