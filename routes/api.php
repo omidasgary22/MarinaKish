1822ad1e0516473ef7caedfc729345ee85eba472
unknown <mohammadelia30@gmail.com>
2024-07-20 13:56:46 +0330

    add permission to route

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,6 @@ Route::post('register',[UserController::class,'create'])->name('register');
 Route::post('login',[UserController::class,'login'])->name('login');
 
 Route::middleware('auth:sanctum')->controller(UserController::class)->prefix('users')->as('users.')->group(function(){
-    Route::get('/index','index')->name('index');
+    Route::get('/index','index')->middleware('permission:user.index')->name('index');
+    Route::get('/ME','me')->middleware('permission:me')->name('dashboard');
 });
