f953d768bc47f4b67a162daf7499a1d4d6fdb4c9
unknown <mohammadelia30@gmail.com>
2024-07-20 12:51:19 +0330

    make permission seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = Role::create(['name' => 'user']);
+        $admin = Role::create(['name' => 'admin']);
+        $user_index = Permission::create(['name' => 'user index']);
+        $user_index->assignRole('admin','user');
+    }
+}
