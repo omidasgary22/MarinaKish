1472383ac64de8eb26c14a4baa0af93a9201b41c
unknown <mohammadelia30@gmail.com>
2024-07-20 12:12:57 +0330

    edit table name

--- database/migrations/2024_07_08_071816_create_comments_table.php
+++ database/migrations/2024_07_08_071816_create_comments_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('comment', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('product_id');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('comment');
+        Schema::dropIfExists('comments');
     }
 };
