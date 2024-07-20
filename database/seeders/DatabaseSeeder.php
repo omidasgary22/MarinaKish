1db28f253145afe3d2ddb22ae09ab8b8deffa42f
unknown <mohammadelia30@gmail.com>
2024-07-20 14:42:29 +0330

    add factory seeder to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([PermissionSeeder::class]);
+        $this->call([PermissionSeeder::class,FactorySeeder::class]);
     }
 }
