0abe889977a06e9c1cd5cfd76e0fc94469895235
unknown <mohammadelia30@gmail.com>
2024-07-20 12:39:28 +0330

    make user index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -23,9 +24,22 @@ class UserController extends Controller
         $token = $user->createToken($code)->plainTextToken;
         return response()->json(['token'=>$token]);
     }
-    public function create(RegisterRequest $request)
+    public function create(Request $request)
     {
         $User = User::create($request->merge(["password" => Hash::make($request->password)])->toArray());
         return response()->json($User);
+    }
+    public function index()
+    {
+        dd(Auth::id());
+        $user = new User();
+        if (Auth::user()){
+            if ($user->hasRole('admin')) {
+                $users = $user->with('comments', 'orders')->orderBy('desc')->paginate(10);
+            }elseif ($user->hasRole('user')){
+                $users = $user->with('comments', 'orders')->where('id',Auth::id())->first();
+            }
+        }
+        return response()->json($users);
     }
 }
