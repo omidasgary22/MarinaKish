a31e1965dc9fe5ecf409b0aacd166935cbe4e868
unknown <mohammadelia30@gmail.com>
2024-07-20 12:14:06 +0330

    edit fillable and hashing password

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'username',
+        'name',
         'email',
         'phone',
         'birth_day',
@@ -44,7 +44,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        //'password' => 'hashed'
+        'password' => 'hashed'
     ];
     public function comments(): HasMany
     {
