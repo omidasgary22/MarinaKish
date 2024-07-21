d554b388933d1568e25d8c58c6f2b6e5b98f7c5d
unknown <omidasgary966@gmail.com>
2024-07-21 14:05:11 +0330

    edit controller for user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,12 +8,4 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function create(Request $request)
-    {
-        $User = User::create($request->toArray());
-        return response()->json($User);
-
-    }
-
-
 }
