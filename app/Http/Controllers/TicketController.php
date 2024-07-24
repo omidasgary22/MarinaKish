189132aa1e43373d9520a4d421db4178222c8a40
unknown <omidasgary966@gmail.com>
2024-07-24 15:03:55 +0330

    added store function in Ticket controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -12,4 +12,10 @@ class TicketController extends Controller
         $tickets = Ticket::all();
         return response()->json(['tickets' => $tickets], 200);
     }
+
+    public function store($request)
+    {
+        $ticket = Ticket::create($request->all());
+        return response()->json(['message' => 'تیکت با موفقیت ایجاد شد', 'ticket' => $ticket], 201);
+    }
 }
