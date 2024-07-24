ba6e6e98c5ff693992a1b8f54ddb3b4b20d01e28
mohammad <mohammadelia30@gmail.com>
2024-07-24 19:30:45 +0330

    shorting code and fox some  bug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,35 +8,41 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index($id)
     {
-        $products = Product::all();
+        $products = new Product();
+        if (!$id) {
+            $products = Product::all();
+        }
+        $products = $products->find($id);
         return response()->json($products);
     }
-
     public function store(StoreProductRequest $request)
     {
-        $product = Product::create($request->toArray());
+        $product = new Product();
+        $product = $product->create($request->toArray());
         return response()->json(['message' => 'محصول با موفقیت ایجاد شد', 'product' => $product]);
     }
-
     public function update($request, $id)
     {
-        $product = Product::findOrFail($id);
+        $product = new Product();
+        $product = $product->find($id);
         $product->update($request->validated());
         return response()->json(['message' => 'محصول با موفقیت به روز رسانی شد', 'product' => $product]);
     }
 
     public function destroy($id)
     {
-        $product = Product::findOrFail($id);
+        $product = new Product();
+        $product = $product->find($id);
         $product->delete();
         return response()->json(['محصول با موفقیت حذف شد ']);
     }
 
     public function restore($id)
     {
-        $product = Product::withTrashed()->findOrFail($id);
+        $product = new Product();
+        $product = $product->withTrashed()->find($id);
         $product->restore();
         return response()->json(['message' => ' محصول با موفقیت بازگردانده شد ', 'product' => $product]);
     }
