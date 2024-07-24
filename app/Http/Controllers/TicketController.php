f3341e0d372e4342a0a807c6a9ae7244e47f7e5b
unknown <omidasgary966@gmail.com>
2024-07-24 15:02:34 +0330

    added index function in Ticket controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function index()
+    {
+        $tickets = Ticket::all();
+        return response()->json(['tickets' => $tickets], 200);
+    }
 }
