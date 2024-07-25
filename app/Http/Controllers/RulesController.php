dec71202651a1d1d392d65f078075f1ab4607e27
unknown <omidasgary966@gmail.com>
2024-07-25 17:11:43 +0330

    creation index function To RulesCintroller

--- app/Http/Controllers/RulesController.php
+++ app/Http/Controllers/RulesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rules;
 use Illuminate\Http\Request;
 
 class RulesController extends Controller
 {
-    //
+    public function index()
+    {
+        $rules = Rules::all();
+        return response()->json(['rules' => 'rules']);
+    }
 }
