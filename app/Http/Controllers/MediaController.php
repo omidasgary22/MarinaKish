d8bbfd2360e3e0b831c61fabf862899b73fa45e4
mohammad <mohammadelia30@gmail.com>
2024-07-27 17:52:00 +0330

    add media for user,product and blog

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MediaController extends Controller
 {
-    //
+    public function save_image(Request $request,$model,$id = null)
+    {
+        switch ($model) {
+            case 'user':
+                $model = new User();
+                $model = $model->find(Auth::id());
+                $profile = $model->addMedia($request->profile)->toMediaCollection('profile');
+                return response()->json(['profile' =>$profile]);
+            case 'product':
+                $model = new Product();
+                $model = $model->find($id);
+                $image = $model->addMedia($request->image)->toMediaCollection('product');
+                return response()->json(['image'=>$image]);
+            case 'blog':
+                $model = new Blog();
+                $model = $model->find($id);
+                $image = $model->addMedia($request->image)->toMediaCollection('blog');
+                return response()->json(['image'=>$image]);
+        }
+    }
 }
