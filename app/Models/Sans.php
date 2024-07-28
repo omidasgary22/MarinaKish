2cb8eb5facc77f4668494acadff86f3805d3b816
mohammad <mohammadelia30@gmail.com>
2024-07-28 11:57:33 +0330

    make relation and fillable

--- app/Models/Sans.php
+++ app/Models/Sans.php
@@ -2,29 +2,33 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Sans extends Model
 {
     use HasFactory;
     protected $fillable =
         [
-            'title',
+            'start_time',
             'remaining',
-            'reserved',
             'product_id'
         ];
     protected $casts =
         [
-            'title' => 'string',
+            'start_time' => 'time',
             'remaining' => 'integer',
-            'reserved' => 'integer',
             'product_id' => 'integer'
         ];
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+    public function orders() :HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
