901a2547bead1416e2fa3882727d0b4f57adea84
mohammad <mohammadelia30@gmail.com>
2024-07-30 23:06:46 +0330

    make controller for sans

---
+++ app/Http/Controllers/SansController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sans;
+use Illuminate\Http\Request;
+
+class SansController extends Controller
+{
+    static function store($time,$pending,$total,$start_time,$ended_at,$id)
+    {
+        while ($start_time->lessThan($ended_at)) {
+            Sans::create([
+                'product_id' => $id,
+                'start_time' => $start_time->toTimeString(),
+                'remaining' => $total,
+            ]);
+            $start_time->addMinute($pending + $time);
+        }
+    }
+}
