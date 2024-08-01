a13552b5dd64fcba21638050d34e014af6c00ac5
unknown <omidasgary966@gmail.com>
2024-08-01 11:16:16 +0330

    add softdelete To question  migration

--- database/migrations/2024_07_09_151216_create_questions_table.php
+++ database/migrations/2024_07_09_151216_create_questions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->text('question');
             $table->text('answer');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
