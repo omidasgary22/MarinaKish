c02b2fcb77a8cbc3497a16df61574718e19affd6
unknown <omidasgary966@gmail.com>
2024-08-01 11:20:22 +0330

    add index function to question contoller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function index()
+    {
+        $faqs = Question::all();
+        return response()->json(['faqs' => $faqs]);
+    }
 }
