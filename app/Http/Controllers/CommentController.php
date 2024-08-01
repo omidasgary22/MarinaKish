c317330808e45a5b26eaef5605bb9cbbf3404f7d
unknown <omidasgary966@gmail.com>
2024-08-01 10:29:21 +0330

    add index function To comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function index()
+    {
+        $comments = Comment::with('user', 'product')->get();
+        return response()->json(['comments' => $comments]);
+    }
 }
