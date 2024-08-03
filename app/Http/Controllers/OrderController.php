f2032b7b7b8410c7bf998f4bee659c9519a08ef6
mohammad <mohammadelia30@gmail.com>
2024-08-03 23:26:41 +0330

    make store function for order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,16 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\Product;
+use App\Models\Sans;
+use http\Env\Response;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
 
     public function store(Request $request)
     {
+        $sans = new Sans();
         $order = new Order();
-        $number_reserved= $order->withSum('number')->where('product_id',$request->product_id)->where('create_at',$request->date)->get();
-        $product = Product::select('id','total')->where('id',$request->product_id);
-        dd($product,$number_reserved);
+        $reserved = $sans->withSum("orders","number",function (Builder $query)use($request){
+            $query->where('order.day_reserved',$request->day_reserved);
+        })->first();
+        $total = $reserved->capacity;
+        $order_sum = $reserved->orders_sum_number;
+        $remaining =  $total - $order_sum;
+        if ($request->number <= $remaining){
+            $order = $order->create($request->merge(['user_id'=>Auth::id()])->toArray());
+            $order_id = $order->id;
+            $product_id = $request->product_id;
+            $number = $request->number;
+            FactorController::store($order_id,$product_id,$number);
+            $order = with('factor')->find($order_id);
+            return response()->json(["message"=>'سفارش با موفقیت ثبت شد',$order]);
+        }else{
+            return response()->json(["message"=>"ظرفیت پر است"]);
+        }
     }
 }
