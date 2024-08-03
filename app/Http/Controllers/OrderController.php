d55f923fd0ae553ef5fb7e6a2cc1981a7d031777
mohammad <mohammadelia30@gmail.com>
2024-08-04 01:44:16 +0330

    fix problem for order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -17,20 +17,21 @@ class OrderController extends Controller
     {
         $sans = new Sans();
         $order = new Order();
-        $reserved = $sans->withSum("orders","number",function (Builder $query)use($request){
-            $query->where('order.day_reserved',$request->day_reserved);
-        })->first();
-        $total = $reserved->capacity;
-        $order_sum = $reserved->orders_sum_number;
+        $reserved = $order->where('day_reserved',$request->day_reserved)->where('sans_id',$request->sans_id)->sum('number');
+        $capacity = $sans->find($request->sans_id);
+        $total = $capacity->capacity;
+        $order_sum = (int)$reserved;
         $remaining =  $total - $order_sum;
         if ($request->number <= $remaining){
             $order = $order->create($request->merge(['user_id'=>Auth::id()])->toArray());
             $order_id = $order->id;
+            $order = Order::find($order_id);
+            $order->passengers()->attach($request->passengers_id);
             $product_id = $request->product_id;
             $number = $request->number;
             FactorController::store($order_id,$product_id,$number);
-            $order = with('factor')->find($order_id);
-            return response()->json(["message"=>'سفارش با موفقیت ثبت شد',$order]);
+            $order = Order::with('factor')->find($order_id);
+            return response()->json(["message"=>'سفارش با موفقیت ثبت شد',"order"=>$order]);
         }else{
             return response()->json(["message"=>"ظرفیت پر است"]);
         }
