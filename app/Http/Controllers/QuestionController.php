efa3535c949818ec7a5f0e5a9eff49e263591c97
mohammad <mohammadelia30@gmail.com>
2024-08-06 17:42:36 +0330

    edit faq controller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -14,32 +14,11 @@ class QuestionController extends Controller
         $faqs = Question::all();
         return response()->json(['faqs' => $faqs]);
     }
-
-    public  function store(StoreQuestionRequest $request)
-    {
-        $faq = Question::create($request->toArray());
-        return response()->json(['message' => 'سوال با موفقیت ایجاد شد', 'faq' => $faq], 201);
-    }
-
     public function update(UpdateQuestionRequest $request, $id)
     {
         $faq = Question::findorFail($id);
         $faq->update($request->toArray());
-        return response()->json(['message' => 'سوال با موفقیت به روز رسانی شد.'], 200);
-    }
-
-    public function destroy($id)
-    {
-        $faq = Question::findOrFail($id);
-        $faq->delete();
-        return response()->json(['message' => 'سوال با موفقیت حذف شد.'], 200);
-    }
-
-    public function restore($id)
-    {
-        $faq = Question::onlyTrashed()->findOrFail($id);
-        $faq->restore();
-        return response()->json(['message' => 'سوال با موفقیت بازیابی شد.'],200);
+        return response()->json(['message' => 'سوال با موفقیت به روز رسانی شد.']);
     }
 
 }
