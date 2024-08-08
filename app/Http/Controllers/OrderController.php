6818f6f9b937bcc67b06773df5e22e70f6c43d97
mohammad <mohammadelia30@gmail.com>
2024-08-08 14:17:24 +0330

    make cancel function

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -54,15 +54,11 @@ class OrderController extends Controller
             return response()->json(["message" => "ظرفیت پر است"]);
         }
     }
-    public function update(Request $request, $id)
+    public function destroy($id)
     {
-        $product_id = $request->product_id;
-        $number = $request->number;
         $order = new Order();
         $order = $order->with('factor')->findOrFail($id);
-        $order->update($request->toArray());
-        $order->passengers()->sync($request->passengers_id);
-        FactorController::update($id, $product_id, $number);
+        $order->delete();
         return response()->json([
             'message' => 'سفارش شما با موفقیت ویرایش شد',
             'order' => $order
