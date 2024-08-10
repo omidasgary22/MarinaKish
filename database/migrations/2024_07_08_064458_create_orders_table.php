4a089107e9e124cd0696bde00f14f88eccda67c9
mohammad <mohammadelia30@gmail.com>
2024-08-11 01:36:35 +0330

    edit colun in table

--- database/migrations/2024_07_08_064458_create_orders_table.php
+++ database/migrations/2024_07_08_064458_create_orders_table.php
@@ -13,9 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
             $table->integer('product_id');
-            $table->unsignedBigInteger('factor_id')->nullable();
             $table->foreignId('factor_id')->references('id')->on('factors');
             $table->foreignId('user_id')->references('id')->on('users');
             $table->integer('number');
