cb711c20bd16bf6ee188c8c7ae60788fb5e671fd
mohammad <mohammadelia30@gmail.com>
2024-08-10 11:50:13 +0330

    delete soft delete for update sans

--- database/migrations/2024_07_27_180839_create_sans_table.php
+++ database/migrations/2024_07_27_180839_create_sans_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->integer('capacity');
             $table->unsignedBigInteger('product_id');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
