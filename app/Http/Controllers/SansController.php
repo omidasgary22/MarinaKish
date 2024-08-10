4c4e3495a38970f388f6281206bef1abba86acff
mohammad <mohammadelia30@gmail.com>
2024-08-10 11:51:33 +0330

    make update function

--- app/Http/Controllers/SansController.php
+++ app/Http/Controllers/SansController.php
@@ -18,10 +18,17 @@ class SansController extends Controller
             $start_time->addMinute($pending + $time);
         }
     }
-    static function update($time, $pending, $total, $start_time,$id, $ended_at)
+    static function update($time, $pending, $total, $start_time, $id, $ended_at)
     {
         $sans = new Sans();
-        $sans = $sans->where('product_id',$id)->get();
-        dd($sans);
+        $sans = $sans->where('product_id', $id)->delete();
+        while ($start_time->lessThan($ended_at)) {
+            Sans::create([
+                'product_id' => $id,
+                'start_time' => $start_time->toTimeString(),
+                'capacity' => $total,
+            ]);
+            $start_time->addMinute($pending + $time);
+        }
     }
 }
