63c7c0bb58092fb65b22fc09ee8ab4f517cac023
mohammad <mohammadelia30@gmail.com>
2024-08-11 17:09:14 +0330

    make store function for passengers

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Passenger;
 use Illuminate\Http\Request;
 
 class PassengerController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $passenger = new Passenger();
+        $passenger = $passenger->onlyTrash()->where('national_code', $request->national_code)->first();
+        if (!$passenger) {
+            $passenger = $passenger->created($request->toArray());
+        }else{
+            $passenger->restore();
+            $passenger->update($request->toArray());
+        }
+        return response()->json($passenger);
+    }
 }
