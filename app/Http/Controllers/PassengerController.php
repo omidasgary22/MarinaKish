90c66c021f8cc25a4377fffc40749ccc7a089b52
mohammad <mohammadelia30@gmail.com>
2024-08-12 00:49:47 +0330

    make index for passengers

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Passenger;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PassengerController extends Controller
 {
+    public function index($id = null)
+    {
+        if (!$id) {
+            $passengers = Passenger::where('user_id',Auth::id())->orderBy('created_at', 'desc')->paginate(10);
+        }else{
+            $passengers = Passenger::find($id);
+        }
+        return response()->json($passengers);
+    }
     public function store(Request $request)
     {
         $passenger = new Passenger();
         $passenger = $passenger->onlyTrash()->where('national_code', $request->national_code)->first();
         if (!$passenger) {
-            $passenger = $passenger->created($request->toArray());
+            $passenger = $passenger->created($request->merge(["user_id"=>Auth::id()])->toArray());
         }else{
             $passenger->restore();
             $passenger->update($request->toArray());
