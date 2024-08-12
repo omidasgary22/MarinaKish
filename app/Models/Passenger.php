1b8a596216f787889d3c538faf708196f9853a86
mohammad <mohammadelia30@gmail.com>
2024-08-12 15:32:42 +0330

    add soft delete to model

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -6,30 +6,33 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Passenger extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable =
-        [
-            'name',
-            'national_code',
-            'gender',
-            'birth_day',
-            'phone',
-        ];
+    [
+        'user_id',
+        'name',
+        'national_code',
+        'gender',
+        'birth_day',
+        'phone',
+    ];
     protected $casts = [
+        'user_id'=> 'integer',
         'birth_day' => 'date',
         'name' => 'string',
         'national_code' => 'string',
         'gender' => 'string',
         'phone' => 'string',
     ];
-    public function users():BelongsTo
+    public function users(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-    public function orders():BelongsToMany
+    public function orders(): BelongsToMany
     {
         return $this->belongsToMany(Order::class, 'orders_passengers', 'passenger_id', 'order_id');
     }
