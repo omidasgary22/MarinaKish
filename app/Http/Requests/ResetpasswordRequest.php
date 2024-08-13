5dbe60796df7fadcf093adf4ae316eb98b55d343
mohammad <mohammadelia30@gmail.com>
2024-08-13 15:57:02 +0330

    make reset password validation

--- app/Http/Requests/ResetpasswordRequest.php
+++ app/Http/Requests/ResetpasswordRequest.php
@@ -22,7 +22,18 @@ class ResetpasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'old_password' => 'required|exists:users,password',
+            'new_password' => 'required|min:8|max:20'
+        ];
+    }
+    public function messages()
+    {
+        return [
+        'old_password.required' => 'رمز قبلی اجباری است',
+        'old_password.exist' => 'رمز عبور قبلی اشتباه است',
+        'new_password.required' => 'رمز عبور جدید اجباری است',
+        'new_password.min' => 'رمز عبور جدیدباید بیشتر از ۸ کارکتر باشد',
+        'new_password.max' => 'طول رمز عبور بیشتر از حد مجاز است'
         ];
     }
 }
