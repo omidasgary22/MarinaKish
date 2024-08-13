18d907102ce92e2c76b5d3bf6ece044b25f68c7d
mohammad <mohammadelia30@gmail.com>
2024-08-13 15:26:06 +0330

    add expire at to code

--- database/migrations/2024_08_13_075155_create_veriving_codes_table.php
+++ database/migrations/2024_08_13_075155_create_veriving_codes_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('phone')->nullable();
             $table->integer('code');
             $table->string('email')->nullable();
+            $table->dateTime('expire_at')->nullable();
             $table->timestamps();
         });
     }
