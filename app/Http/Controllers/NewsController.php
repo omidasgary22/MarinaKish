b2be19b0da62496ea1776f363666945337947ed9
mohammad <mohammadelia30@gmail.com>
2024-08-14 22:48:28 +0330

    make news for send off code

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\News;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class NewsController extends Controller
+{
+   public function index()
+   {
+       $news = News::all()->sortByDesc('created_at')->paginate(10);
+       return response()->json(['news_member'=>$news]);
+   }
+   public function store(Request $request)
+   {
+       $restore = News::onlyTrashed()->where('email',$request)->first();
+       if ($restore)
+       {
+           $restore->restore();
+       }else {
+           News::created($request);
+       }
+       return response()->json(['news_member'=>'شما با موفقیت در خبرنامه عضو شدید']);
+   }
+   static function send($code)
+   {
+       $members = News::all()->toArray();
+       foreach ($members as $member) {
+           Mail::to($member['email'])->send(new News($code));
+       }
+   }
+   public function delete(Request $request)
+   {
+       News::where('email',$request['email'])->delete();
+       return response()->json('شما از خبر نامه خارج شدید');
+   }
+}
