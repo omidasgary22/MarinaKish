cf41928299f09e80f85869ace06c1310ed4ec80b
mohammad <mohammadelia30@gmail.com>
2024-08-19 18:59:04 +0330

    make setting controller

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    //
+    public function index()
+    {
+        $settings = Setting::all();
+        return response()->json(["setting"=>$settings]);
+    }
+    public function update(Request $request)
+    {
+        $setting = Setting::find($request->keys())->updated($request->all());
+        return response()->json($setting);
+    }
 }
