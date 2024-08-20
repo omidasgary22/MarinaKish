e6ce9c8366650b1c6a043c027f332fbb0c02a414
mohammad <mohammadelia30@gmail.com>
2024-08-20 15:59:29 +0330

    make transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Factor;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+        public static function store($id)
+        {
+            $price = Factor::find($id)->total_price;
+            $token = "توکن اختصاصی ";
+            $args = [
+                "amount" =>$price,
+        "payerIdentity" => "شناسه کاربر در صورت وجود",
+        "payerName" => "نام کاربر پرداخت کننده",
+        "description" => "توضیحات",
+        "returnUrl" => "آدرس برگشتی از سمت درگاه",
+        "clientRefId" => "شماره فاکتور"
+    ];
+
+    $payment = new \PayPing\Payment($token);
+
+    try {
+        $payment->pay($args);
+    } catch (\Exception $e) {
+        var_dump($e->getMessage());
+    }
+    //echo $payment->getPayUrl();
+
+    header('Location: ' . $payment->getPayUrl());
+        }
 }
