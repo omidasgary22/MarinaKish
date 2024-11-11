379f52939ba8c70b93f1f707b618357b8a46c3c8
mohammad <mohammadelia30@gmail.com>
2024-11-11 19:37:35 +0330

    add softdelete to blog

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Blog extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia;
+    use HasFactory, InteractsWithMedia,softDeletes;
     protected $fillable = [
         'title',
         'summary',
